eda992d504b73c1f84e6257dd3d2e3b0242b7c14
Iuliia Tsaplina <zhydanovajulia@gmail.com>
2019-07-26 01:20:29 +0300

    setup bootstrap and jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
+//= require jquery-ui
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
