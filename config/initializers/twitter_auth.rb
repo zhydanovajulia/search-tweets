0bbc0cd6410ffb7d84f9595696b45dae94b71a53
Iuliia Tsaplina <zhydanovajulia@gmail.com>
2019-07-26 01:35:56 +0300

    setup twitter auth keys

---
+++ config/initializers/twitter_auth.rb
@@ -0,0 +1,6 @@
+require 'twitter'
+
+CLIENT = Twitter::REST::Client.new do |config|
+  config.consumer_key    = "Da0h9oQNmoei2g9WcEyoHAuTP"
+  config.consumer_secret = "Lz654zMUMHxpRkfkDT3IPYBsdLHahg8RqA4HiRdMgqoURJZbXI"
+end
