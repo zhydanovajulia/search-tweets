5d532eb960262b859e1b09a46fba43e0e48c78e2
Iuliia Tsaplina <zhydanovajulia@gmail.com>
2019-07-26 01:38:47 +0300

    search tweets flow with bootstrap markup

---
+++ app/controllers/search_tweets_controller.rb
@@ -0,0 +1,36 @@
+class SearchTweetsController < ApplicationController
+
+  def index
+    @hashtags = {}
+    @hashtags.default = 0
+    @top_hashtags = {}
+    @found_tweets = []
+    @query = search_params[:query]
+
+    if @query.present?
+      @found_tweets = CLIENT.search(@query, result_type: search_params[:result_type]).take(100)
+
+      collect_hashtags(@found_tweets)
+      hash_get_top_hashtags
+    end
+  end
+
+
+  private
+
+  def search_params
+    params.permit(:query, :result_type)
+  end
+
+  def collect_hashtags(tweets)
+    tweets.each do |tweet|
+      if tweet.hashtags?
+        tweet.hashtags.each { |hashtag| @hashtags[hashtag.text] += 1 }
+      end
+    end
+  end
+
+  def hash_get_top_hashtags(top=10)
+    @top_hashtags = @hashtags.sort_by(&:last).reverse.take(top).to_h
+  end
+end
